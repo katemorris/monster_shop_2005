d940987299467a19ff3b56afe20f75da847492bf
Kate Tester <katemorris@gmail.com>
2020-11-06 19:47:10 -0700

    Adds sad path to ensure merchant cant see other merchant discounts

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "As a merchant" do
       @discount_5 = BulkDiscount.create!(name: "5 for 5", percent_off: 5, min_amount: 5, merchant_id: @brian.id)
     end
 
-    it 'I see a link to add a new bulk discount for my items' do
+    it 'I see a link to view my bulk discounts' do
       visit "/merchant"
 
       expect(page).to have_link("Bulk Discounts")
@@ -38,6 +38,15 @@ RSpec.describe "As a merchant" do
         expect(page).to have_link('Edit')
         expect(page).to have_link('Delete')
       end
+    end
+
+    it "I don't see other merchant's discounts" do
+      competitor = Merchant.create(name: "Competitor", address: '19584 Whatever Ln', city: 'Denver', state: 'CO', zip: 80210)
+      not_my_discount = BulkDiscount.create!(name: "Bad Idea", percent_off: 100, min_amount: 1, merchant_id: competitor.id)
+
+      visit merchant_bulk_discounts_path
+      expect(page).to have_content(@discount_5.name)
+      expect(page).to_not have_content(not_my_discount.name)
     end
   end
 end
